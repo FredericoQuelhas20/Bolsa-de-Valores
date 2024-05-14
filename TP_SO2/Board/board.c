9e28ba95967a9ec55fd68f14f1b379cf75c8fe7f
Frederico Quelhas <166937271+FredericoQuelhas20@users.noreply.github.com>
2024-05-14 22:48:04 +0100

    Update board.c

--- TP_SO2/Board/board.c
+++ TP_SO2/Board/board.c
@@ -9,11 +9,9 @@
 #define TAM_STR 100
 #define MAX_EMPRESAS 30
 #define NEMPRESAS_DISPLAY 10
-#define NOME_SM		    _T("memória")
+#define NOME_SM		    _T("memÃ³ria")
 #define NOME_MUTEX_IN   _T("mutex_in")
 #define NOME_MUTEX_OUT  _T("mutex_out")
-#define NOME_SEM_L	    _T("sem_livres")
-#define NOME_SEM_O      _T("sem_ocupados")
 
 typedef struct {
 	TCHAR nomeEmp[TAM_STR];
@@ -22,52 +20,46 @@ typedef struct {
 } Empresa;
 
 typedef struct {
-	Empresa empresas[MAX_EMPRESAS];
-}ArrayEmpresa;
-
-//typedef struct {
-//	DWORD in, out;
-//	Empresa empresas[TAM];
-//	DWORD indiceUltimaTransacao, numEmpresas;
-//} SDATA;
-
-typedef struct {
 	BOOL continua;
 	DWORD nDisplayEmp;
 	HANDLE hMutex, hEv;
 	Empresa empresas[MAX_EMPRESAS];
-	//SDATA* shm;
+	DWORD pauseComando;
 } TDATA;
 
 DWORD WINAPI atualizaTopEmpresas(LPVOID data) {
 
 	TDATA* td = (TDATA*)data;
-	//Empresa emp[MAX_EMPRESA];
-	//ArrayEmpresa emp;
 	DWORD i = 0, pos = 0;
 	HANDLE hEv = OpenEvent(EVENT_MODIFY_STATE, FALSE, _T("Event"));
 	HANDLE hMutex = OpenMutex(SYNCHRONIZE, FALSE, _T("Mutex"));
 	Empresa* pEmpresas;
 	do {
+		if (td->pauseComando != 0) {
 
+			Sleep(td->pauseComando * 1000);
+			
+		}
+
+		td->pauseComando = 0;
 		WaitForSingleObject(hEv, INFINITE);
 		WaitForSingleObject(hMutex, INFINITE);
 
 		HANDLE hMap = OpenFileMapping(FILE_MAP_WRITE, FALSE, _T("Empresa"));
-		pEmpresas = (Empresa*)MapViewOfFile(hMap, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, sizeof(Empresa) * NEMPRESAS_DISPLAY);
+		pEmpresas = (Empresa*)MapViewOfFile(hMap, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, sizeof(Empresa));
 
 		system("cls");
-		_tprintf(_T("Ações Mais Valiosas e Última Transação:\n"));
+		_tprintf(_T("AÃ§Ãµes Mais Valiosas e Ãšltima TransaÃ§Ã£o:\n"));
 		_tprintf(_T("-------------------------------------------------------\n"));
 
 		for (DWORD i = 0; i < td->nDisplayEmp; i++) {
 			if (_tcslen(pEmpresas[i].nomeEmp) > 0) {
-				_tprintf_s(_T("Empresa: %s, Ações: %u, Valor por Ação: %u\n"), pEmpresas[i].nomeEmp, pEmpresas[i].numAcoes, pEmpresas[i].valorAcao);
+				_tprintf_s(_T("Empresa: %s, AÃ§Ãµes: %u, Valor por AÃ§Ã£o: %u\n"), pEmpresas[i].nomeEmp, pEmpresas[i].numAcoes, pEmpresas[i].valorAcao);
 			}
 		}
 
 		//_tprintf(_T("-------------------------------------------------------\n"));
-		//_tprintf_s(_T("Última Transação: %s, Ações: %d, Valor: %i\n"),
+		//_tprintf_s(_T("Ãšltima TransaÃ§Ã£o: %s, AÃ§Ãµes: %d, Valor: %i\n"),
 		//	pEmpresas[td->shm->indiceUltimaTransacao].nomeEmp,
 		//	pEmpresas[td->shm->indiceUltimaTransacao].numAcoes,
 		//	pEmpresas[td->shm->indiceUltimaTransacao].valorAcao);
@@ -92,6 +84,7 @@ int _tmain(int argc, TCHAR* argv[]) {
 	TCHAR str[40];
 	BOOL primeiro = FALSE;
 	DWORD displays = 0;
+	td.pauseComando = 0;
 
 #ifdef UNICODE
 	_setmode(_fileno(stdin), _O_WTEXT);
@@ -120,12 +113,12 @@ int _tmain(int argc, TCHAR* argv[]) {
 	td.nDisplayEmp = displays;
 	td.continua = TRUE;
 	hThread = CreateThread(NULL, 0, atualizaTopEmpresas, &td, 0, NULL);
-	_tprintf(_T("Criei a Thread"));
 	do {
 		_tscanf_s(_T("%s"), str, 40 - 1);
 		_tprintf(_T("Estou no loop"));
 	} while (_tcscmp(str, _T("fim")) != 0);
 	td.continua = FALSE;
+	_tprintf(_T("\n [BOARD] fechando...	\n"));
 	WaitForSingleObject(hThread, INFINITE);
 	CloseHandle(td.hMutex);
 	CloseHandle(td.hEv);
